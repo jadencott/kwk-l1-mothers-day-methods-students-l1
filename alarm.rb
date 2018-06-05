0505c1e2408a56dc055073fbb06239140dd5c95e
Jaden <jcottorone21405@gmail.com>
2018-06-05 19:38:47 +0000

    automatically backed up by learn

--- alarm.rb
+++ alarm.rb
@@ -3,11 +3,11 @@
 # otherwise, if the time is between 3 pm to 10pm say good evening
 # otherwise say goodnight 
 
-time.now.hour
 
-puts "what time is it?"
-time_npw=gets.chomp
-if the time.now.hour is >= 6 && time.now.hour is <= 11 
+
+
+if the Time.now.hour is >= 6 && Time.now.hour <= 11 
   puts "Good Morning America"
-elsif time.now.hour is >= 11 && time.now.hour <= 3
+elsif Time.now.hour is >= 11 && Time.now.hour <= 15
 puts "Good Afternoon"
+elsif Time.now.hour is >
